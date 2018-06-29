a465a089b7fcc97e68617f9dee427e9c8ae38112
aagusalim <agusalim_a@yahoo.com>
2018-06-28 22:30:06 -0400

    checkpoint 18 interfaces:completed

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -15,6 +15,8 @@ album.songs.forEach( (song,index) => {
 		`);
 	song.element.on('click',event => {
 		player.playPause(song);
+		$('button#play-pause').attr('playState',player.playState);
+		
 	});
 $('#song-list').append(song.element);
 });
