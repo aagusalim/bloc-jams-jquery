e67fc7d210414cb9238afb3aeb6dfc7c5e1871c0
aagusalim <agusalim_a@yahoo.com>
2018-06-28 23:42:59 -0400

    Assigment 18 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,11 +3,22 @@
 		player.playPause();
 		$(this).attr('playState',player.playState);
 	});
-	$('button#next').on ('click',function(){
+
+	$('button#previous').on ('click',function(){
+		if(player.playState !=='playing'){ return; }
+
+		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+const nextSongIndex = currentSongIndex - 1
+if (nextSongIndex >= album.songs.length) { return; }
+const nextSong = album.songs[nextSongIndex];
+player.playPause(nextSong);
+});
+		$('button#next').on ('click',function(){
 		if(player.playState !=='playing'){ return; }
 
 		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
 const nextSongIndex = currentSongIndex + 1
+if (nextSongIndex >= album.songs.length) { return; }
 const nextSong = album.songs[nextSongIndex];
 player.playPause(nextSong);
 	});
@@ -18,11 +29,12 @@ $('#time-control input').on('input', function (event) {
 });
 
 		setInterval( () => {
-	if (player.playState !== 'playing') { return; }
-	const currentTime = player.getTime();
-	const duration = player.getDuration();
-	const percent = (currentTime / duration) * 100;
-	$('#time-control .current-time').text (currentTime);
-	$('#time-control input').val(percent);
-}, 1000);
+			if (player.playState !== 'playing') { return; }
+			const currentTime = player.getTime();
+			const duration = player.getDuration();
+			const percent = (currentTime / duration) * 100;
+			$('#time-control .current-time').text (currentTime);
+			$('#time-control input').val(percent);
+		}, 1000);
 }
+
