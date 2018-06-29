dcf29c3194ceab8c2e54996e4286534dc5803928
aagusalim <agusalim_a@yahoo.com>
2018-06-28 23:42:59 -0400

    Assigment 18 complete

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -3,12 +3,24 @@
 		player.playPause();
 		$(this).attr('playState',player.playState);
 	});
-	$('button#next').on ('click',function(){
+
+	$('button#previous').on ('click',function(){
+		if(player.playState !=='playing'){ return; }
+
+		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+const nextSongIndex = currentSongIndex - 1
+if (nextSongIndex >= album.songs.length) { return; }
+const nextSong = album.songs[nextSongIndex];
+player.playPause(nextSong);
+});
+		$('button#next').on ('click',function(){
 		if(player.playState !=='playing'){ return; }
 
 		const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
 const nextSongIndex = currentSongIndex + 1
+if (nextSongIndex >= album.songs.length) { return; }
 const nextSong = album.songs[nextSongIndex];
 player.playPause(nextSong);
-	});
+});
+	
 }
